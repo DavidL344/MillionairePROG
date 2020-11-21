6134f78537d66bf7be3ce86e1435fd4b0bda4ebd
DavidL344 <69001314+DavidL344@users.noreply.github.com>
2020-11-21 20:25:29 +0100

    Implement CSV Reading

--- MillionairePROG.py
+++ MillionairePROG.py
@@ -3,6 +3,9 @@
 
 from time import sleep
 from sys import exit
+from os import path
+
+csv_questions = []
 
 def clearScreen():
     from os import system, name
@@ -53,7 +56,7 @@ def convertVar(var, datatype):
     except:
         return var
 
-def EncoderTool(endecode, encodeType, value):
+def EncoderTool(endecode, encodeType, value = "./questions", returnWhere = "memory"):
     import base64
     if (endecode == "encode"):
         if (encodeType == "base64"):
@@ -61,6 +64,18 @@ def EncoderTool(endecode, encodeType, value):
             base64_bytes = base64.b64encode(value_bytes)
             value_base64 = EncoderTool("decode", "UTF-8", base64_bytes)
             return value_base64
+        elif (encodeType == "csv"):
+            # value = "./questions"
+            with open(value + ".csv", 'r') as csv_file:
+                in_memory_file = csv_file.read()
+                encoded_file = EncoderTool("encode", "base64", in_memory_file)
+                if (returnWhere == "memory"):
+                    return encoded_file
+                elif (returnWhere == "file"):
+                    # value = ./questions
+                    with open(value + ".bin", 'w') as bin_file:
+                        bin_file.writelines(encoded_file)
+            return
         else:
             return value.encode(encoding=encodeType, errors='strict')
     elif (endecode == "decode"):
@@ -69,24 +84,68 @@ def EncoderTool(endecode, encodeType, value):
             value_bytes = base64.b64decode(base64_bytes)
             value_text = EncoderTool("decode", "UTF-8", value_bytes)
             return value_text
+        elif (encodeType == "csv"):
+            # value = ./questions
+            with open(value + ".bin", 'r') as bin_file:
+                in_memory_file = bin_file.read()
+                decoded_file = EncoderTool("decode", "base64", in_memory_file)
+                if (returnWhere == "memory"):
+                    return decoded_file
+                elif (returnWhere == "file"):
+                    # ./questions
+                    with open(value + ".csv", 'w') as csv_file:
+                        csv_file.writelines(decoded_file)
+            return
         else:
             return value.decode(encoding=encodeType, errors='strict')
     else:
         return value
 
+def loadQuestions():
+    # Load file in memory: https://stackoverflow.com/a/17767445
+    clearScreen()
+    import csv
+    if (path.exists("./questions.bin")):
+        in_memory_file = EncoderTool("decode", "csv")
+        csv_questions = csv.reader(in_memory_file.splitlines(), delimiter=",", quotechar="|")
+
+        # Test start
+        # TODO: ignore row[0] - it is used as a header of the table
+        for row in csv_questions:
+            string_no_quotation_marks = row[0][1:-1]
+            print(string_no_quotation_marks)
+        # Test end
+
+        print("Game, Start!")
+    else:
+        print("Otázky nejsou k nalezení! Je třeba je nejprve importovat.")
+    sleep(3)
+
+def convertQuestions():
+    clearScreen()
+    if (path.exists("./questions.csv")):
+        print("Probíhá importování otázek ze souboru questions.csv")
+        EncoderTool("encode", "csv", "./questions", "file")
+        print("Import dokončen")
+    else:
+        print("Soubor questions.csv neexistuje!")
+    sleep(3)
+
+def loadTheGame():
+    loadQuestions()
+    
+    #encode = EncoderTool("decode", "base64", "RGF2aWQ=")
+    #print(encode)
+    
+
 def main():
     while(True):
-        menu = choice(["Kdo chce být milionářem - PROG edition\r\n", "1 - Hrát\r\n2 - Odejít"], [1, 2])
-
-        if (menu == 1):
-            break
-        elif (menu == 2):
+        menu = choice(["Kdo chce být milionářem - PROG edition\r\n", "1 - Hrát\r\n2 - Odejít\r\n# - Importovat otázky"], ['1', '2', '#'], str)
+        if (menu == '1'):
+            loadTheGame()
+        elif (menu == '2'):
             exit(0)
-        else:
-            # Should never occue
-            continue
-    print("Game, Start!")
-    encode = EncoderTool("decode", "base64", "RGF2aWQ=")
-    print(encode)
-
+        elif (menu == '#'):
+            convertQuestions()
+        continue
 main()
